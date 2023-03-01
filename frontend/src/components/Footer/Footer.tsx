52f5081dc74eb6eeb67310c8b24e5b8a61c9ee9f
Tabea Schaeffer <tabea@briese-schaeffer.de>
2023-03-01 13:23:15 +0100

    Week Overview implementation

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import styles from './Footer.module.scss'
 
 export default function Footer() {
   return (
-    <div className={styles.test}>Footer</div>
+    <></>
+    // <div className={styles.test}>Footer</div>
   )
 }
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
+import Image from 'next/image';
+import logo from '../../../public/logo.svg';
+import styles from '../Header/Header.module.scss';
+
 export default function Header() {
   return (
-    <div>Header</div>
+    <>
+      <div className={styles.container}>
+        <div className='grid grid-cols-3'>
+          <Image src={logo} alt="Logo" width={200} height={155} priority />
+          <div className='col-span-2'>
+            <h1>Hi, Jo!</h1>
+            <h2 className={styles.secondTitle}>Willkommen bei Tasty Plan</h2>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
