80ffaff121238c4e95523652bd969c29c631d8f1
fhs49302 <fhs49302@fh-salzburg.ac.at>
2023-03-01 10:57:36 +0100

    hintergrund

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import Layout from '@components/Layout/Layout'
-import '@styles/globals.scss'
-import type { AppProps } from 'next/app'
+import Layout from '@components/Layout/Layout';
+import '@styles/globals.scss';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-        <Component {...pageProps} />
-    </Layout> 
-  )
+    return (
+        <Layout>
+            <div className='bg-[url("../../public/startBackground.jpg")]'>
+                <Component {...pageProps} />
+            </div>
+        </Layout>
+    );
 }
