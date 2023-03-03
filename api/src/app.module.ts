6343360803cb93ca82cda314de72dd5f5bbff469
Markus Fichtner <markus.fichtner1999@gmail.com>
2023-03-03 10:07:24 +0100

    cleaned

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IngredientsModule } from './ingredients/ingredients.module';
 import { RecipesModule } from './recipes/recipes.module';
-import { StepsModule } from './steps/steps.module';
 import { PreferencesController } from './preferences/preferences.controller';
 import { PreferencesModule } from './preferences/preferences.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -11,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
 import { WeekplanModule } from './weekplan/weekplan.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), IngredientsModule, RecipesModule, StepsModule, PreferencesModule, PrismaModule, WeekplanModule],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), IngredientsModule, RecipesModule, PreferencesModule, PrismaModule, WeekplanModule],
     controllers: [AppController, PreferencesController],
     providers: [AppService],
 })
