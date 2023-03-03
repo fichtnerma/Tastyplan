0c90c18ea9feb0d3d711f08172fa7f927eb98583
Tabea Schaeffer <tabea@briese-schaeffer.de>
2023-03-03 10:56:59 +0100

    refactoring Preferences

--- frontend/src/pages/intolerances/index.tsx
+++ frontend/src/pages/intolerances/index.tsx
@@ -1,16 +1,27 @@
 import styles from '@styles/Intolerances.module.scss';
+
+import logo from '../../../public/logo.svg';
+
+import Image from 'next/image';
 import Link from 'next/link';
+
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 export default function IntolerancesPage() {
     const intolerances = [
-        'Laktoseintoleranz',
-        'Zöliakie',
-        'Histaminintoleranz',
-        'Sorbitintoleranz',
-        'Sacharoseintoleranz',
-        'Alkoholintoleranz',
+        'Erdnuss',
+        'Haselnus',
+        'Walnuss',
+        'Schalenfrucht',
+        'Soja',
+        'Gluten',
+        'Fruktose',
+        'Ei',
+        'Laktose',
+        'Schalentiere',
+        'Fisch',
+        'Alkohol'
     ];
 
     const [choices, setChoices] = useState<string[]>([]);
@@ -56,47 +67,52 @@ export default function IntolerancesPage() {
     };
 
     return (
-        <div className="flex justify-center items-center">
-            <form className="flex justify-center py-8 px-12 w-[36rem] bg-white rounded-[20px]">
-                <fieldset className="flex flex-col">
-                    <h2 className="text-5xl font-semibold text-gray-custom4 mb-14">Unverträglichkeiten</h2>
-                    <div className="grid grid-cols-3 gap-3 mb-6">
-                        {intolerances.map((intolerance, i) => (
-                            <div key={i} className={styles.intoleranceWrapper}>
-                                <input
-                                    type="checkbox"
-                                    name="intolerances"
-                                    value={intolerance}
-                                    checked={choices.includes(intolerance)}
-                                    onChange={onAddChoice}
-                                />
-                                <label htmlFor={intolerance}>{intolerance}</label>
-                            </div>
-                        ))}
-                    </div>
-                    <div className="flex justify-between relative">
-                        <Link className="font-medium text-gray-custom4" href={'/preferences'}>
-                            Go Back
-                        </Link>
-                        <button
-                            type="submit"
-                            className="font-medium text-gray-custom4"
-                            data-btn="skip"
-                            onClick={handleClick}
-                        >
-                            Skip Question
-                        </button>
-                        <button
-                            type="submit"
-                            className="font-medium text-gray-custom4"
-                            data-btn="next"
-                            onClick={handleClick}
-                        >
-                            Next Step
-                        </button>
-                    </div>
-                </fieldset>
-            </form>
+        <div>
+            <div className={styles.logo}>
+                <Image src={logo} className='ml-24 fixed' alt="logo" width={200} priority />
+            </div>
+            <div className="flex justify-center items-center">
+                <form className="flex justify-center py-8 px-12 w-[36rem] bg-white rounded-[20px]">
+                    <fieldset className="flex flex-col">
+                        <h2 className="text-3xl font-semibold text-gray-custom4 mb-8">Was sind deine Unverträglichkeiten?</h2>
+                        <div className="grid grid-cols-4 gap-4 mb-6">
+                            {intolerances.map((intolerance, i) => (
+                                <div key={i} className={styles.intoleranceWrapper}>
+                                    <input
+                                        type="checkbox"
+                                        name="intolerances"
+                                        value={intolerance}
+                                        checked={choices.includes(intolerance)}
+                                        onChange={onAddChoice}
+                                    />
+                                    <label htmlFor={intolerance}>{intolerance}</label>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="flex justify-between relative">
+                            <Link className="font-medium text-gray-custom4" href={'/preferences'}>
+                                Zurück
+                            </Link>
+                            <button
+                                type="submit"
+                                className="font-medium text-gray-custom4"
+                                data-btn="skip"
+                                onClick={handleClick}
+                            >
+                                Überspringen
+                            </button>
+                            <button
+                                type="submit"
+                                className="font-medium text-gray-custom4"
+                                data-btn="next"
+                                onClick={handleClick}
+                            >
+                                Weiter
+                            </button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     );
 }
