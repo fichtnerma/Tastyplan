d69f5306d840174d1bdf96487686b8cb524ea218
hanEck <hannes.eckelt@t-online.de>
2023-03-06 17:28:02 +0100

    feature(images) => Add static folder on server side for image storage

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,10 +7,21 @@ import { PreferencesController } from './preferences/preferences.controller';
 import { PreferencesModule } from './preferences/preferences.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { WeekplanModule } from './weekplan/weekplan.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), IngredientsModule, RecipesModule, PreferencesModule, PrismaModule, WeekplanModule],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), 
+        IngredientsModule, 
+        RecipesModule, 
+        PreferencesModule, 
+        PrismaModule, 
+        WeekplanModule,
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'images'),
+            serveRoot: '/images',
+          }),],
     controllers: [AppController, PreferencesController],
     providers: [AppService],
 })
