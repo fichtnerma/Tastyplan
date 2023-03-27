b013a91df46fcdefd7f2d68d7efd02f8d2a185fc
fhs49302 <fhs49302@fh-salzburg.ac.at>
2023-03-27 15:53:54 +0200

    implemented new design for food lifestyle

--- frontend/src/pages/preferences/index.tsx
+++ frontend/src/pages/preferences/index.tsx
@@ -9,14 +9,14 @@ import React, { useState } from 'react';
 
 const PreferencesPage = () => {
     const preferences = [
-        { food: 'omnivor', description: 'You eat all animal products' },
-        { food: 'flexitarier', description: 'You rarely eat all animal products' },
-        { food: 'pescetarier', description: 'You only eat fish from all animal products' },
-        { food: 'vegetarisch', description: 'You dont eat any meat and fish' },
+        { food: 'omnivore', description: 'You eat all animal products' },
+        { food: 'flexitarian', description: 'You rarely eat all animal products' },
+        { food: 'pescetarian', description: 'You only eat fish from all animal products' },
+        { food: 'vegetarian', description: 'You dont eat any meat and fish' },
         { food: 'vegan', description: 'You dont eat any kind of animal products' },
     ];
 
-    const [selection, setSelection] = useState('omnivor');
+    const [selection, setSelection] = useState('omnivore');
 
     const onChoiceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSelection(e.target.value);
@@ -39,34 +39,36 @@ const PreferencesPage = () => {
         <div>
             <Image src={logo} className="ml-24 mb-8" alt="logo" width={200} priority />
             <div className="flex justify-center items-center ml-50">
-                <form className="flex justify-center py-8 px-12 w-5/6 h-full bg-white rounded-[20px]">
-                    <fieldset className="flex flex-col w-4/5 mt-20">
-                        <h4 className="mb-8">Welcher Ernährungstyp bist du?</h4>
-                        <div className={styles.preferencesWrapper}>
-                            {preferences.map((preference, i) => (
-                                <div key={i} className={styles.choiceWrapper}>
-                                    <input
-                                        type="radio"
-                                        name="preferences"
-                                        value={preference.food}
-                                        checked={selection === preference.food}
-                                        onChange={onChoiceChange}
-                                    />
-                                    <label htmlFor={preference.food}>
-                                        <p className="absolute min-w-full pb-4 pl-6">{preference.food}</p>
-                                        <p className="text-xs min-w-full pt-8">{preference.description}</p>
-                                    </label>
-                                </div>
-                            ))}
+                <form className="flex justify-center py-8 px-12 h-70v w-5/6 bg-white rounded-[20px]">
+                    <fieldset className="flex flex-col w-4/5 mt-24 ">
+                        <h4 className="mb-8">What is your food lifestyle?</h4>
+                        <div className={styles.scrolling}>
+                            <div className={styles.preferencesWrapper}>
+                                {preferences.map((preference, i) => (
+                                    <div key={i} className={styles.choiceWrapper}>
+                                        <input
+                                            type="radio"
+                                            name="preferences"
+                                            value={preference.food}
+                                            checked={selection === preference.food}
+                                            onChange={onChoiceChange}
+                                        />
+                                        <label htmlFor={preference.food}>
+                                            <p className="absolute min-w-full pb-4 pl-6">{preference.food}</p>
+                                            <p className="text-xs min-w-full pt-8">{preference.description}</p>
+                                        </label>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                        <div className="flex justify-center relative">
+                        <div className="flex justify-center relative my-8">
                             <a
-                                className="absolute top-0 right-0 font-medium text-gray-custom4"
+                                className="btn-submit absolute top-0 right-0 font-medium text-gray-custom4"
                                 href="/preferences"
                                 onClick={onSubmitSelection}
                                 data-anchor="next"
                             >
-                                Weiter
+                                Next
                             </a>
                         </div>
                     </fieldset>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
             },
             height: {
                 '90v': '90vh',
+                '70v': '70vh',
             },
         },
     },
