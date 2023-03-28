14dc96ff6cf1d6bd9edc18d0aeec942d1305cff7
fhs49068 <mpross.mmt-m2022@fh-salzburg.ac.at>
2023-03-28 12:50:01 +0200

    FIX: enable dynamic routing

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -5,20 +5,21 @@ function Login({ onSubmit }: { onSubmit: () => void }) {
     const [password, setPassword] = useState('');
 
     return (
-        <div className="p-12 form-container sign-in-container">
-            <form action="#">
+        <div className="basis-1/2 form-container sign-in-container">
+            <form action="#" className="flex flex-col items-center w-full px-12">
                 <h1>Sign in</h1>
-                <span>or use your account</span>
-                <div className="text-input-wrapper mb-6">
+                <div className="w-full text-input-wrapper mb-6">
                     <label htmlFor="accountName">E-Mail Adress or User Name</label>
                     <input type="text" name="accountName" />
                 </div>
-                <div className="text-input-wrapper mb-12">
+                <div className="w-full text-input-wrapper mb-12">
                     <label htmlFor="password">Password</label>
                     <input type="password" name="password" />
                 </div>
-                <a href="#">Forgot your password?</a>
-                <button>Sign In</button>
+                <a href="#" className="mb-4">
+                    Forgot your password?
+                </a>
+                <button className="btn-primary">Sign In</button>
             </form>
         </div>
     );
--- frontend/src/pages/authentication/[slug].tsx
+++ frontend/src/pages/authentication/[slug].tsx
@@ -13,20 +13,44 @@ function AuthenticationPage() {
     useEffect(() => {
         if (slug === 'login') setPageState('login');
         else if (slug === 'registration') setPageState('registration');
-    }, []);
+    }, [router]);
+
+    // useEffect(() => {
+    //     router.push(`${router.basePath}/authentication/${pageState}`, undefined, undefined);
+    // }, [pageState]);
 
     const handleLoginSubmit = () => {
         console.log('login button clicked');
     };
 
+    const handleSignUp = () => {
+        router.push(`${router.basePath}/authentication/registration`, undefined, undefined);
+        setPageState('registration');
+    };
+
+    const handleSignIn = () => {
+        router.push(`${router.basePath}/authentication/login`, undefined, undefined);
+        setPageState('login');
+    };
+
     return (
         <div className="flex justify-center">
-            {pageState === 'login' ? <Login onSubmit={handleLoginSubmit} /> : <Register />}
-            <div className="bg-green-custom2 p-5">
-                <h2 className="h2-white">Hello, Friend!</h2>
-                <p className="p-white text-sm mb-8">Enter your personal details and start your journey with us</p>
-                <button className="btn-secondary block my-0 mx-auto">Sign up</button>
-            </div>
+            {pageState === 'login' ? (
+                <>
+                    <Login onSubmit={handleLoginSubmit} />{' '}
+                    <div className="flex flex-col items-center justify-center bg-green-custom2 px-12">
+                        <h2 className="h2-white">Hello, Friend!</h2>
+                        <p className="p-white text-sm mb-8">
+                            Enter your personal details and start your journey with us
+                        </p>
+                        <button className="btn-secondary block my-0 mx-auto" onClick={handleSignUp}>
+                            Sign up
+                        </button>
+                    </div>{' '}
+                </>
+            ) : (
+                <Register onSignIn={handleSignIn} />
+            )}
         </div>
     );
 }
