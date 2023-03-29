19239a78b5232a208dab5c452e6e43a3db66ac06
fhs49302 <fhs49302@fh-salzburg.ac.at>
2023-03-29 17:43:48 +0200

    added search input

--- frontend/src/components/Dislikes/Dislikes.tsx
+++ frontend/src/components/Dislikes/Dislikes.tsx
@@ -1,21 +1,38 @@
+import { useState } from 'react';
+
 type OnBackFunction = () => void;
 interface DislikesProps {
     onBack: OnBackFunction;
 }
 
 export default function Dislikes({ onBack }: DislikesProps) {
+    const [dislike, setDislike] = useState('Search');
+
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         onBack();
     };
     return (
-        <div className="flex justify-between relative">
-            <button type="submit" className="btn-primary mt-10" data-btn="back" onClick={handleClick}>
-                Back
-            </button>
-            <button type="submit" className="btn-primary mt-10" data-btn="next" onClick={handleClick}>
-                Next
-            </button>
+        <div>
+            <h4 className="mb-8">What food do you dislike?</h4>
+            <div className="h-[300px] overflow-y-auto">
+                <div className="flex">
+                    <div className="text-input-wrapper w-1/2 mr-16">
+                        <input type="text" name="search" value={dislike} onChange={(e) => setDislike(e.target.value)} />
+                    </div>
+                    <button type="submit" className="btn-primary">
+                        Go
+                    </button>
+                </div>
+            </div>
+            <div className="flex justify-between relative">
+                <button type="submit" className="btn-primary mt-10" data-btn="back" onClick={handleClick}>
+                    Back
+                </button>
+                <button type="submit" className="btn-primary mt-10" data-btn="next" onClick={handleClick}>
+                    Next
+                </button>
+            </div>
         </div>
     );
 }
