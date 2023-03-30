6489971151b1f87eaf152a7523c467ff54fa5c35
fhs49068 <manuel@DESKTOP-VFB6ELB>
2023-03-30 09:57:52 +0200

    revert

--- frontend/src/components/Dislikes/Dislikes.tsx
+++ frontend/src/components/Dislikes/Dislikes.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styles from '../Dislikes/Dislikes.module.scss';
 
 type OnBackFunction = () => void;
 interface DislikesProps {
@@ -6,23 +7,53 @@ interface DislikesProps {
 }
 
 export default function Dislikes({ onBack }: DislikesProps) {
-    const [dislike, setDislike] = useState('Search');
+    const dislikes = ['Potato', 'Salmon', 'Zucchini', 'Carrot', 'Peas', 'Chicken', 'Spinach', 'Cauliflower', 'Cream'];
+
+    const [allDislikes, setDislike] = useState('Search');
 
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         onBack();
     };
+
+    const onDeleteChoice = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log('Deleted', e.target.value);
+    };
     return (
         <div>
             <h4 className="mb-8">What food do you dislike?</h4>
-            <div className="h-[300px] overflow-y-auto">
+            <div className="h-[300px]">
                 <div className="flex">
                     <div className="text-input-wrapper w-1/2 mr-16">
-                        <input type="text" name="search" value={dislike} onChange={(e) => setDislike(e.target.value)} />
+                        <input
+                            type="text"
+                            name="search"
+                            value={allDislikes}
+                            onChange={(e) => setDislike(e.target.value)}
+                        />
                     </div>
                     <button type="submit" className="btn-primary">
                         Go
                     </button>
+                </div>{' '}
+                <div className="grid grid-cols-4 gap-4 my-4 overflow-y-auto">
+                    {dislikes.map((dislike, i) => (
+                        <div key={i} className={styles.dislikeWrapper}>
+                            <div className={styles.containerField}>
+                                <input
+                                    type="button"
+                                    name="dislikes"
+                                    value={dislike}
+                                    checked={allDislikes.includes(dislike)}
+                                    onChange={onDeleteChoice}
+                                />
+
+                                <label htmlFor={dislike}>
+                                    <p>{dislike}</p>
+                                </label>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className="flex justify-between relative">
