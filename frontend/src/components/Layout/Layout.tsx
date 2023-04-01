0c655d36cc2956e11a8819b9a512f85f8d356cec
fhs49068 <manuel@DESKTOP-VFB6ELB>
2023-04-01 15:01:02 +0200

    WIP

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -4,8 +4,16 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import styles from './Layout.module.scss';
 
+import { Inter, Bebas_Neue } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'], style: ['normal'], weight: ['200', '400', '700'] });
+const bebasNeue = Bebas_Neue({ subsets: ['latin'], style: 'normal', weight: '400' });
+
+console.log(inter, bebasNeue);
+
 export default function Layout({ children }: React.PropsWithChildren) {
     const { asPath } = useRouter();
+
     return (
         <>
             {asPath !== '/' &&
@@ -18,11 +26,11 @@ export default function Layout({ children }: React.PropsWithChildren) {
                 style={{
                     backgroundColor:
                         asPath == '/' ||
-                            asPath == '/authentication/registration' ||
-                            asPath == '/authentication/login' ||
-                            asPath == '/preferences' ||
-                            asPath.includes('/intolerances') ||
-                            asPath.includes('/setup')
+                        asPath == '/authentication/registration' ||
+                        asPath == '/authentication/login' ||
+                        asPath == '/preferences' ||
+                        asPath.includes('/intolerances') ||
+                        asPath.includes('/setup')
                             ? 'var(--green-light)'
                             : 'var(--white)',
                 }}
