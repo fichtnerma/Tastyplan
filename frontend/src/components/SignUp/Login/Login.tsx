8c8516a57799db8b58ebd894c208033d7a09b486
Markus Fichtner <markus.fichtner1999@gmail.com>
2023-04-19 14:18:34 +0200

    change btn to submit

--- frontend/src/components/SignUp/Login/Login.tsx
+++ frontend/src/components/SignUp/Login/Login.tsx
@@ -47,7 +47,7 @@ export default function Login({ visible }: LoginProps) {
                     label="Password"
                 />
                 <a href="#">Forgot your password?</a>
-                <button className='btn-primary'>Sign In</button>
+                <input type='submit' className='btn-primary'>Sign In</input>
             </form>
         </div>
     );
