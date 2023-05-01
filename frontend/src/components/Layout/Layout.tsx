89bc6d9fd48f6fa149a22b89fbfe9a48a455da34
fhs49068 <mpross.mmt-m2022@fh-salzburg.ac.at>
2023-05-01 18:34:34 +0200

    WIP

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -30,7 +30,7 @@ export default function Layout({ children }: React.PropsWithChildren) {
             >
                 {children}
             </main>
-            <Footer />
+            {/* <Footer /> */}
         </>
     );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                inter: ['var(--font-inter)', ...fontFamily.sans],
-                bebas: ['var(--font-bebas)', ...fontFamily.sans],
-                zeyada: ['var(--font-zeyada)', ...fontFamily.sans],
+                inter: ['var(--font-inter)'],
+                bebas: ['var(--font-bebas)'],
+                zeyada: ['var(--font-zeyada)'],
             },
             colors: {
                 green: {
