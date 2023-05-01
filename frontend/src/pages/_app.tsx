441fee8aea1ff9cb5b1232ea47277820d278cefe
fhs49068 <mpross.mmt-m2022@fh-salzburg.ac.at>
2023-05-01 10:39:03 +0200

    FEAT: add button to page end

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,16 +3,17 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
-import { Inter, Bebas_Neue } from '@next/font/google';
+import { Inter, Bebas_Neue, Zeyada } from '@next/font/google';
 import Layout from '@components/Layout/Layout';
 
 const inter = Inter({ subsets: ['latin'], style: ['normal'], weight: ['200', '400', '700'], variable: '--font-inter' });
 const bebasNeue = Bebas_Neue({ subsets: ['latin'], style: 'normal', weight: '400', variable: '--font-bebas' });
+const zeyada = Zeyada({ subsets: ['latin'], style: 'normal', weight: '400', variable: '--font-zeyada' });
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <SessionProvider session={pageProps.session}>
-            <div className={`${inter.variable} ${bebasNeue.variable}`}>
+            <div className={`${inter.variable} ${bebasNeue.variable} ${zeyada.variable}`}>
                 <Layout>
                     <Component {...pageProps} />
                 </Layout>
