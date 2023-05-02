fb9b21712e73b2d11e1ba1289c95ac77a5138699
fhs49302 <fhs49302@fh-salzburg.ac.at>
2023-05-02 14:32:26 +0200

    added icons to allergies

--- frontend/src/components/Intolerances/Intolerances.tsx
+++ frontend/src/components/Intolerances/Intolerances.tsx
@@ -1,15 +1,6 @@
 import { useState } from 'react';
-import Image from 'next/image';
+import Icon from '@components/Icon/Icon';
 import styles from '../Intolerances/Intolerances.module.scss';
-import gluten from '../../../public/Icons/Weizen.svg';
-import walnut from '../../../public/Icons/Wallnuss.svg';
-import soy from '../../../public/Icons/Soja.svg';
-import shellfish from '../../../public/Icons/Shrimp.svg';
-import laktose from '../../../public/Icons/Milch_Käse.svg';
-import hazelnut from '../../../public/Icons/Haselnuss.svg';
-import fish from '../../../public/Icons/Fisch.svg';
-import peanut from '../../../public/Icons/Erdnuss.svg';
-import egg from '../../../public/Icons/Ei.svg';
 
 type OnNextFunction = () => void;
 type OnBackFunction = () => void;
@@ -24,22 +15,22 @@ interface IntolerancesProps {
 
 export default function Intolerances({ onNext, onBack, onChoice, allergens }: IntolerancesProps) {
     const intolerances = [
-        { ui: 'Peanuts', code: 'peanut', icon: peanut },
-        { ui: 'Hazelnuts', code: 'hazelnut', icon: hazelnut },
-        { ui: 'Walnuts', code: 'walnut', icon: walnut },
-        { ui: 'Other Nuts', code: 'shellFruit', icon: hazelnut },
-        { ui: 'Lactose', code: 'milk', icon: laktose },
-        { ui: 'Gluten', code: 'gluten', icon: gluten },
-        { ui: 'Eggs', code: 'egg', icon: egg },
-        { ui: 'Shellfish', code: 'crustacaen', icon: shellfish },
-        { ui: 'Fish', code: 'fish', icon: fish },
-        { ui: 'Soy', code: 'soy', icon: soy },
-        { ui: 'Celery', code: 'celery', icon: hazelnut },
-        { ui: 'Mustard', code: 'mustard', icon: hazelnut },
-        { ui: 'Sesame', code: 'sesame', icon: hazelnut },
-        { ui: 'Sulfur Dioxide', code: 'sulfur', icon: hazelnut },
-        { ui: 'Lupine', code: 'lupine', icon: hazelnut },
-        { ui: 'Mollusk', code: 'mollusk', icon: hazelnut },
+        { ui: 'Peanuts', code: 'peanut', icon: 'Erdnuss' },
+        { ui: 'Hazelnuts', code: 'hazelnut', icon: 'Haselnuss' },
+        { ui: 'Walnuts', code: 'walnut', icon: 'Wallnuss' },
+        { ui: 'Other Nuts', code: 'shellFruit', icon: 'Nüsse' },
+        { ui: 'Lactose', code: 'milk', icon: 'laktose' },
+        { ui: 'Gluten', code: 'gluten', icon: 'Weizen' },
+        { ui: 'Eggs', code: 'egg', icon: 'Ei' },
+        { ui: 'Shellfish', code: 'crustacaen', icon: 'Shrimp' },
+        { ui: 'Fish', code: 'fish', icon: 'fisch' },
+        { ui: 'Soy', code: 'soy', icon: 'Soja' },
+        { ui: 'Celery', code: 'celery', icon: 'Sellerie' },
+        { ui: 'Mustard', code: 'mustard', icon: 'Senfglas' },
+        { ui: 'Sesame', code: 'sesame', icon: 'Sesam' },
+        { ui: 'Sulfur Dioxide', code: 'sulfur', icon: 'Wein' },
+        { ui: 'Lupine', code: 'lupine', icon: 'Lupinen' },
+        { ui: 'Mollusk', code: 'mollusk', icon: 'Weichtiere' },
     ];
 
     const [allergeneChoices, setAllergeneChoices] = useState(allergens);
@@ -88,13 +79,9 @@ export default function Intolerances({ onNext, onBack, onChoice, allergens }: In
                                     <label htmlFor={intolerance.ui}>
                                         <p className="text-base">{intolerance.ui}</p>
                                     </label>
-                                    <Image
-                                        src={intolerance.icon}
-                                        className="absolute z-[91] top-1"
-                                        alt="icon"
-                                        width={60}
-                                        priority
-                                    />
+                                    <div className="absolute z-[91] top-4 left-2">
+                                        <Icon size={40} icon={intolerance.icon}></Icon>
+                                    </div>
                                 </span>
                             </div>
                         </div>
