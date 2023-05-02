8308c0ada97d091d2d693747bce82f50ca25b5fb
fhs49068 <mpross.mmt-m2022@fh-salzburg.ac.at>
2023-05-02 15:44:00 +0200

    FEAT: add custom checkbox

--- frontend/src/components/FormInputs/Checkbox.tsx
+++ frontend/src/components/FormInputs/Checkbox.tsx
@@ -1,15 +1,15 @@
+import styles from './Checkbox.module.scss';
+
 type CheckboxProps = {
     label: string;
 };
 
 function Checkbox({ label }: CheckboxProps) {
     return (
-        <>
-            <label htmlFor={label}>
-                <input type="checkbox" name={label} />
-                {label}
-            </label>
-        </>
+        <div className={styles.checkboxContainer}>
+            <input type="checkbox" name={label} id={label} />
+            <label htmlFor={label}>{label}</label>
+        </div>
     );
 }
 
