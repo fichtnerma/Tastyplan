05acfbfa714070b4478dc6bdb8c822ba250903bd
Tabea Schaeffer <tabea@briese-schaeffer.de>
2023-05-02 14:07:31 +0200

    change portion implementation

--- frontend/src/components/IngredientList/IngredientList.tsx
+++ frontend/src/components/IngredientList/IngredientList.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Ingredient } from 'src/types/types';
+import styles from './IngredientList.module.scss';
 
 type IngredientListProps = {
     ingredients: Array<Ingredient> | undefined;
 };
 
 function IngredientList({ ingredients }: IngredientListProps) {
+    const [portion, setPortion] = useState(1);
+
+    const changePortion = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const clickedButton = e.currentTarget.getAttribute('data-anchor');
+        if (clickedButton == '+') {
+            setPortion(portion + 1);
+        } else {
+            if (portion > 1) {
+                setPortion(portion - 1);
+            }
+        }
+    };
     return (
         <div>
             <div className="flex mb-5">
-                <button type="button" className="btn-primary mt-10" data-btn="back">
+                <button
+                    type="button"
+                    className={`btn-primary ${styles.btnPortion} mr-2`}
+                    onClick={changePortion}
+                    data-anchor={'-'}
+                >
                     -
                 </button>
-                <p>1</p>
-                <button type="button" className="btn-primary mt-10" data-btn="back">
+                <p id="portion">{portion}</p>
+                <button
+                    type="button"
+                    className={`btn-primary ${styles.btnPortion} ml-2 mr-5`}
+                    onClick={changePortion}
+                    data-anchor={'+'}
+                >
                     +
                 </button>
                 <h5>Portionen</h5>
@@ -23,7 +46,7 @@ function IngredientList({ ingredients }: IngredientListProps) {
                 {ingredients?.map((ingredient) => (
                     <div key={'test'} className="grid grid-cols-3 gap-5 mb-2">
                         <p className="text-right col-span-1 font-semibold">
-                            {ingredient.quantity} {ingredient.unit}
+                            {ingredient.quantity * portion} {ingredient.unit}
                         </p>
                         <p className="text-left col-span-2 mr-2">{ingredient.ingredient.name}</p>
                     </div>
