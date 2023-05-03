ee5ea885a18ab08f5405071096971e381d5e2821
manuel.pross <manuel.pross@flipapp.de>
2023-05-03 10:08:36 +0200

    FEAT: implement radio group

--- frontend/src/components/FormInputs/Radiobutton.tsx
+++ frontend/src/components/FormInputs/Radiobutton.tsx
@@ -1,5 +1,25 @@
-function RadioButton() {
-    return <div></div>;
+import { CustomRadioBtn } from 'src/types/types';
+import styles from './Radiobutton.module.scss';
+
+type RadiobuttonProps = {
+    groupName: string;
+    radioBtn: CustomRadioBtn;
+    handleChange: (id: string) => void;
+};
+
+function RadioButton({ radioBtn, groupName, handleChange }: RadiobuttonProps) {
+    return (
+        <div className={styles.checkboxContainer}>
+            <input
+                type="radio"
+                name={groupName}
+                id={radioBtn.id}
+                onChange={() => handleChange(radioBtn.id)}
+                checked={radioBtn.checked}
+            />
+            <label htmlFor={radioBtn.id}>{radioBtn.label}</label>
+        </div>
+    );
 }
 
 export default RadioButton;
--- frontend/src/pages/shoppingList/index.tsx
+++ frontend/src/pages/shoppingList/index.tsx
@@ -1,5 +1,5 @@
-import ShoppingList from '@components/ShoppingList/ShoppingList';
-import { Ingredient } from 'src/types/types';
+import RadioGroup from '@components/FormInputs/RadioGroup/RadioGroup';
+import { CustomRadioBtn, Ingredient } from 'src/types/types';
 
 const ingredientsDummy1: Ingredient[] = [
     {
@@ -34,47 +34,25 @@ const ingredientsDummy1: Ingredient[] = [
     },
 ];
 
-const ingredientsDummy2: Ingredient[] = [
-    {
-        ingredient: { name: 'pineapples' },
-        quantity: 150,
-        unit: 'g',
-    },
-    {
-        ingredient: { name: 'apples' },
-        quantity: 5,
-        unit: '',
-    },
-    {
-        ingredient: { name: 'lemons' },
-        quantity: 3,
-        unit: '',
-    },
-    {
-        ingredient: { name: 'letuce' },
-        quantity: 1,
-        unit: '',
-    },
-    {
-        ingredient: { name: 'butter' },
-        quantity: 300,
-        unit: 'g',
-    },
-    {
-        ingredient: { name: 'flour' },
-        quantity: 0,
-        unit: '',
-    },
-];
+const radioBtns: CustomRadioBtn[] = ingredientsDummy1.map((ingredient, index) => {
+    return {
+        id: index + '',
+        label: `${ingredient.quantity} ${ingredient.unit} ${ingredient.ingredient.name} `,
+        checked: false,
+    };
+});
 
 function ShoppingListPage() {
     return (
         <div>
             <h1 className="h1-green">Your shopping list</h1>
             <h2>Things you need to buy</h2>
-            <ShoppingList ingredients={ingredientsDummy1} />
+            {/* <ShoppingList ingredients={ingredientsDummy1} /> */}
             <h2>Things you already have</h2>
-            <ShoppingList ingredients={ingredientsDummy2} />
+            {/* <ShoppingList ingredients={ingredientsDummy2} /> */}
+
+            {/* <RadioButton label="Trololo" /> */}
+            <RadioGroup radioBtns={radioBtns} groupName="ingredients" />
         </div>
     );
 }
