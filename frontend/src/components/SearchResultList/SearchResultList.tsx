f750a19ecdd7fba9ea6fc9053281d7c84153cb61
fhs49302 <fhs49302@fh-salzburg.ac.at>
2023-05-03 10:45:50 +0200

    styling

--- frontend/src/components/SearchResultList/SearchResultList.tsx
+++ frontend/src/components/SearchResultList/SearchResultList.tsx
@@ -7,7 +7,7 @@ interface SearchResultlistProps {
 
 function SearchResultlist({ searchResults, clickHandler }: SearchResultlistProps) {
     return (
-        <ul className="w-1/3 rounded-md bg-gray-custom2 z-[1]" onClick={clickHandler}>
+        <ul className="max-h-72 overflow-y-auto w-1/3 rounded-md bg-gray-custom2 z-[1]" onClick={clickHandler}>
             {searchResults.map((el) => (
                 <li
                     className="px-4 py-1 cursor-pointer border-b border-gray-custom5 hover:bg-gray-custom5 last:border-none last:rounded-b-md first:rounded-t-md first-letter:capitalize"
