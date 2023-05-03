bda09dfcf3864875b3d91ba7e9a083ed10ce0765
fhs49068 <mpross.mmt-m2022@fh-salzburg.ac.at>
2023-05-03 14:03:55 +0200

    FEAT: add filtering of checked ingredients

--- frontend/src/components/FormInputs/Checkbox.tsx
+++ frontend/src/components/FormInputs/Checkbox.tsx
@@ -5,17 +5,21 @@ type CheckboxProps = {
     groupName: string;
     customCheckbox: CustomSelectionInput;
     handleChange: (id: string, checked: boolean) => void;
+    disabled: boolean;
 };
 
-function Checkbox({ groupName, customCheckbox, handleChange }: CheckboxProps) {
+function Checkbox({ groupName, customCheckbox, handleChange, disabled }: CheckboxProps) {
     return (
         <div className={styles.checkboxContainer}>
             <input
                 type="checkbox"
                 name={groupName}
                 id={customCheckbox.id}
-                onChange={() => handleChange(customCheckbox.id, customCheckbox.checked)}
-                checked={customCheckbox.checked}
+                onChange={() => {
+                    if (!disabled) return handleChange(customCheckbox.id, customCheckbox.checked);
+                }}
+                checked={disabled ? true : customCheckbox.checked}
+                disabled={disabled}
             />
             <label htmlFor={customCheckbox.id}>{customCheckbox.label}</label>
         </div>
--- frontend/src/components/FormInputs/CheckboxGroup/CheckboxGroup.tsx
+++ frontend/src/components/FormInputs/CheckboxGroup/CheckboxGroup.tsx
@@ -1,30 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CustomSelectionInput } from 'src/types/types';
 import Checkbox from '../Checkbox';
 
 type CheckboxProps = {
     groupName: string;
     checkboxes: CustomSelectionInput[];
+    onCheckboxSelect?: (id: string) => void;
+    disabled: boolean;
 };
 
-function CheckboxGroup({ checkboxes, groupName }: CheckboxProps) {
-    const [checkboxGroup, setCheckboxGroup] = useState([...checkboxes]);
+function CheckboxGroup({ checkboxes, groupName, onCheckboxSelect, disabled }: CheckboxProps) {
+    const [checkboxGroup, setCheckboxGroup] = useState(checkboxes);
+
+    useEffect(() => {
+        setCheckboxGroup(checkboxes);
+    }, [checkboxes]);
 
     const handleGroupChange = (id: string, checked: boolean) => {
-        console.log(id);
-        const selectionGroupCache = [...checkboxGroup];
-        selectionGroupCache[+id].checked = !checked;
-        setCheckboxGroup(selectionGroupCache);
+        const updatedSelection = [...checkboxGroup].map((selection) => {
+            if (selection.id === id)
+                return {
+                    id: selection.id,
+                    label: selection.label,
+                    checked: !checked,
+                };
+            return selection;
+        });
+        setCheckboxGroup(updatedSelection);
+
+        if (onCheckboxSelect) onCheckboxSelect(id);
     };
 
     return (
-        <div className="w-fit">
+        <div className="w-fit inline-grid grid-cols-1 gap-4">
             {checkboxGroup.map((checkbox) => (
                 <Checkbox
                     customCheckbox={checkbox}
                     key={checkbox.id}
                     groupName={groupName}
                     handleChange={handleGroupChange}
+                    disabled={disabled}
                 />
             ))}
         </div>
