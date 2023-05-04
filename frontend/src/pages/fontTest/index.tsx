f6f7ead7b107a5801fb5ca526cbc963a161abfd7
fhs49068 <mpross.mmt-m2022@fh-salzburg.ac.at>
2023-05-04 08:02:51 +0200

    FEAT: add breakpoints for headings and p

---
+++ frontend/src/pages/fontTest/index.tsx
@@ -0,0 +1,14 @@
+function FontTest() {
+    return (
+        <div>
+            <h1>H1: The quick brown fox jumps over the lazy dog</h1>
+            <h2>H2: The quick brown fox jumps over the lazy dog</h2>
+            <h3>H3: The quick brown fox jumps over the lazy dog</h3>
+            <h4>H4: The quick brown fox jumps over the lazy dog</h4>
+            <h5>H5: The quick brown fox jumps over the lazy dog</h5>
+            <p>P: The quick brown fox jumps over the lazy dog</p>
+        </div>
+    );
+}
+
+export default FontTest;
