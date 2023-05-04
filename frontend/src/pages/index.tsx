ab2b6f3a10913211112042bdaf371145e3858872
Tabea Schaeffer <tabea@briese-schaeffer.de>
2023-05-04 14:49:46 +0200

    added Components

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,22 +1,9 @@
 import { useRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import Steps from '@components/Startpage/Steps/Steps';
+import Benefits from '@components/Startpage/Benefits/Benefits';
 import Icon from '@components/Icon/Icon';
-import Benefits from '@components/Benefits/Benefits';
-
-type Step = {
-    id: number;
-    desc: string;
-    class: string;
-    svg: CustomSVG;
-};
-
-type CustomSVG = {
-    class: string;
-    src: string;
-    width: number;
-    height: number;
-};
 
 export type Benefit = {
     id: number;
@@ -28,86 +15,12 @@ export type Benefit = {
 export default function Index() {
     const fieldRef = useRef<HTMLInputElement>(null);
 
-    const steps: Step[] = [
-        {
-            id: 1,
-            desc: 'Set up your account by entering your dietary preferences, food allergies, and schedule. This will help Tastyplan generate a personalized meal plan for you.',
-            class: 'relative flex items-center max-w-[764px] mr-auto mb-[15rem]',
-            svg: {
-                class: 'absolute top-[-2rem] right-[-24rem]',
-                src: '/Curves/curve1.svg',
-                width: 451,
-                height: 416,
-            },
-        },
-        {
-            id: 2,
-            desc: "Once you've set up your account, Tastyplan will generate a weekly meal plan for you, complete with recipes and a shopping list.",
-            class: 'relative flex items-center max-w-[764px] ml-auto mb-[18rem]',
-            svg: {
-                class: 'absolute top-[8rem] right-[31rem]',
-                src: '/Curves/curve2.svg',
-                width: 814,
-                height: 291,
-            },
-        },
-        {
-            id: 3,
-            desc: 'Review your meal plan and make any necessary adjustments. You can swap out recipes, add or remove meals, and adjust portions as needed.',
-            class: 'relative flex items-center max-w-[764px] mr-auto mb-[16rem]',
-            svg: {
-                class: 'absolute top-[1rem] right-[-25rem]',
-                src: '/Curves/curve3.svg',
-                width: 814,
-                height: 291,
-            },
-        },
-        {
-            id: 4,
-            desc: 'Print or save your shopping list and head to the grocery store to get everything you need for the week.',
-            class: 'relative flex items-center max-w-[764px] ml-auto',
-            svg: {
-                class: '',
-                src: '',
-                width: 0,
-                height: 0,
-            },
-        },
-    ];
-
-    const benefits: Benefit[] = [
-        {
-            id: 1,
-            icon: '/Icons/Ei.svg',
-            heading: 'Saves time and money',
-            desc: 'by allowing for efficient grocery shopping and reducing food waste.',
-        },
-        {
-            id: 2,
-            icon: '/Icons/Ei.svg',
-            heading: 'Promotes healthier eating habits',
-            desc: 'by encouraging the consumption of a balanced and varied diet that meets nutritional needs and supports overall health.',
-        },
-        {
-            id: 3,
-            icon: '/Icons/Ei.svg',
-            heading: 'Reduces food waste',
-            desc: 'by helping you purchase only the necessary ingredients and use up items before they expire.',
-        },
-        {
-            id: 4,
-            icon: '/Icons/Ei.svg',
-            heading: 'Reduces stress and decision fatigue',
-            desc: 'by eliminating the need to constantly think about what to eat and simplifying mealtime preparation.',
-        },
-    ];
-
     const scrollTo = () => {
         fieldRef?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
     };
 
     return (
-        <div className="px-14">
+        <div className="">
             <div className="flex flex-col justify-start h-screen">
                 <div className="flex justify-between items-center">
                     <div className="flex items-center h-[150px]">
@@ -115,14 +28,6 @@ export default function Index() {
                     </div>
                     <Link href="/authentication/login">
                         <div className="flex justify-center bg-green-custom2 h-14 w-14 rounded-full mt-5 hover:bg-green-custome3">
-                            {/* <Image
-                                src={'/Icons/Header/user.png'}
-                                alt="logo"
-                                height={30}
-                                width={30}
-                                priority
-                                className="h-fit pt-[20%] invert"
-                            /> */}
                             <div className="h-fit pt-[20%] invert">
                                 <Icon size={30} icon="user"></Icon>
                             </div>
@@ -160,31 +65,13 @@ export default function Index() {
                     Meal planning has never been easier and more delicious.
                 </p>
             </div>
-            <div>
+            <div className="pt-40">
                 <h2 className="mb-24">But why Meal Planning</h2>
-                <Benefits benefits={benefits} />
+                <Benefits />
             </div>
-            <h2 className="mb-24">How Does Tastyplan work</h2>
-            <div className="flex flex-col w-[1440px] mx-auto my-0">
-                {steps.map((step) => (
-                    <div key={step.id} className={step.class}>
-                        <div className="flex flex-col justify-center items-center mr-8">
-                            <span className="h3-green-dark">0{step.id}</span>
-                            <p className="h4-green-dark mt-[-1rem]">step</p>
-                        </div>
-                        <p>{step.desc}</p>
-                        {step.svg.src !== '' && (
-                            <Image
-                                src={step.svg.src}
-                                alt="curve"
-                                width={step.svg.width}
-                                height={step.svg.height}
-                                priority
-                                className={step.svg.class}
-                            />
-                        )}
-                    </div>
-                ))}
+            <div className="pt-40">
+                <h2 className="mb-24">How Does Tastyplan work</h2>
+                <Steps />
             </div>
         </div>
     );
