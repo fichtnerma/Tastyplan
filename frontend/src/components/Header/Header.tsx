82b3ad3db02f1d33fa1193cfc1985727883c4f6f
Tabea Schaeffer <tabea@briese-schaeffer.de>
2023-05-04 13:50:30 +0200

    line under active tab

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -42,33 +42,46 @@ export default function Header() {
                     </Link>
 
                     <div className="flex gap-10 mr-8 mt-6">
-                        <Link href="/weekOverview" onClick={changeActiveTab} className={`link weekOverview`}>
+                        <Link
+                            href="/weekOverview"
+                            onClick={changeActiveTab}
+                            className={`link weekOverview ${styles.active}`}
+                        >
                             <div className={`flex gap-2 items-center ${styles.weekplan}`}>
                                 <Icon size={25} icon="calender"></Icon>
                                 <p>Weekplan</p>
                             </div>
+                            <div
+                                className={`bg-green-custom2 rounded-full h-1 w-full mt-2 line ${styles.lineHide} ${styles.lineShow}`}
+                            ></div>
                         </Link>
                         <Link href="/shoppingList" onClick={changeActiveTab} className={`link shoppingList`}>
                             <div className={`flex gap-2 items-center ${styles.shoppingList}`}>
                                 <Icon size={25} icon="shoppinglist"></Icon>
                                 <p>Shopping List</p>
                             </div>
+                            <div
+                                className={`bg-green-custom2 rounded-full h-1 w-full mt-2 line ${styles.lineHide}`}
+                            ></div>
                         </Link>
                         <Link href="/cookbook" onClick={changeActiveTab} className={`link cookbook`}>
                             <div className={`flex gap-2 items-center fill-none ${styles.cookbook}`}>
                                 <Icon size={25} icon="heart"></Icon>
                                 <p>Cookbook</p>
                             </div>
+                            <div
+                                className={`bg-green-custom2 rounded-full h-1 w-full mt-2 line ${styles.lineHide}`}
+                            ></div>
                         </Link>
                         <div className={styles.userIcon}>
                             <div className="flex gap-2 items-center">
                                 <Icon size={25} icon="user"></Icon>
                             </div>
-                            <div className={`w-40 pt-8 right-14 absolute ${styles.dropdown}`}>
-                                <div className="rounded-2xl bg-green-custom1 ">
+                            <div className={`w-40 pt-8 right-14 absolute ${styles.dropdown} `}>
+                                <div className={`rounded-2xl bg-green-custom1 ${headerClass}`}>
                                     <Link href="/settings" className="">
                                         <div
-                                            className={`flex gap-2 text-right pt-5 pb-3 pl-5 settings dropdown${styles.settingsDropdown}`}
+                                            className={`flex gap-2 text-right pt-5 pb-3 pl-5 settings dropdown ${styles.settingsDropdown}`}
                                         >
                                             <p>Settings</p>
                                             <div className="right-0 absolute pr-8">
@@ -78,7 +91,7 @@ export default function Header() {
                                     </Link>
                                     <Link href="/" className="">
                                         <div
-                                            className={`flex gap-2 items-center pb-5 pl-5 pt-3 user dropdown${styles.userDropdown}`}
+                                            className={`flex gap-2 items-center pb-5 pl-5 pt-3 user dropdown ${styles.userDropdown}`}
                                         >
                                             <p>Log out</p>
                                             <div className="right-0 absolute pr-8">
@@ -97,9 +110,9 @@ export default function Header() {
 }
 
 function changeActiveTab() {
-    const currentPath = window.location.pathname;
     const activeClass = `${styles.active}`;
     const settingsClass = `${styles.settingsActive}`;
+    const lineShowClass = `${styles.lineShow}`;
     // const userClass = `${styles.userActive}`;
 
     document.querySelectorAll('.link').forEach((el) => {
@@ -111,18 +124,34 @@ function changeActiveTab() {
         // el.classList.remove(userClass);
     });
 
+    document.querySelectorAll('.line').forEach((el) => {
+        el.classList.remove(lineShowClass);
+    });
+
+    const activeElement = getElement(activeClass, settingsClass);
+
+    const line = activeElement?.querySelector('.line');
+    line?.classList.add(lineShowClass);
+}
+
+function getElement(activeClass: string, settingsClass: string) {
+    const currentPath = window.location.pathname;
     if (currentPath === '/weekOverview') {
         const element = document.querySelector('.weekOverview');
         element?.classList.add(activeClass);
+        return element;
     } else if (currentPath === '/shoppingList') {
         const element = document.querySelector('.shoppingList');
         element?.classList.add(activeClass);
+        return element;
     } else if (currentPath === '/cookbook') {
         const element = document.querySelector('.cookbook');
         element?.classList.add(activeClass);
+        return element;
     } else if (currentPath === '/settings') {
         const element = document.querySelector('.settings');
         element?.classList.add(settingsClass);
+        return null;
     }
     // else if (currentPath === '/') {
     //     const element = document.querySelector('.user');
@@ -130,6 +159,7 @@ function changeActiveTab() {
     // }
     else {
         console.log('Header: No Path found');
+        return null;
     }
 }
 
