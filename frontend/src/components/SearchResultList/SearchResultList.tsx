3e77647ce158b5fb0f0dd250c5897f103df301e4
fhs49302 <fhs49302@fh-salzburg.ac.at>
2023-05-05 11:45:59 +0200

    fxid dropdown

--- frontend/src/components/SearchResultList/SearchResultList.tsx
+++ frontend/src/components/SearchResultList/SearchResultList.tsx
@@ -7,10 +7,7 @@ interface SearchResultlistProps {
 
 function SearchResultlist({ searchResults, clickHandler }: SearchResultlistProps) {
     return (
-        <ul
-            className="absolute top-[488px] left max-h-72 overflow-y-auto w-1/6 rounded-md bg-gray-custom2 z-[1]"
-            onClick={clickHandler}
-        >
+        <ul className="w-full max-h-72 overflow-y-auto rounded-md bg-gray-custom2" onClick={clickHandler}>
             {searchResults.map((el) => (
                 <li
                     className="px-4 py-1 cursor-pointer border-b border-gray-custom5 hover:bg-gray-custom5 last:border-none last:rounded-b-md first:rounded-t-md first-letter:capitalize"
