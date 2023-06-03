67502fe8d3ae66fc33e035a7ee276ab80b62f4e9
Markus Fichtner <markus.fichtner1999@gmail.com>
2023-06-03 16:33:11 +0200

    move e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:8080',
+        baseUrl: 'http://193.170.119.139/',
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
