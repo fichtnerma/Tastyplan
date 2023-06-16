8637a2bae3231e22425f46ffa19a63e1f022a908
fhs49068 <mpross.mmt-m2022@fh-salzburg.ac.at>
2023-06-16 21:16:18 +0200

    fix: readd logo on desktop, make logo responsive

--- frontend/src/components/Layout/Modal.tsx
+++ frontend/src/components/Layout/Modal.tsx
@@ -1,8 +1,17 @@
 import React, { PropsWithChildren } from 'react';
+import Image from 'next/image';
 
 export default function Modal({ children }: PropsWithChildren) {
     return (
-        <div className="lg:w-screen lg:h-screen lg:fixed lg:flex-wrap lg:top-0 lg:left-0 lg:flex lg:justify-center lg:content-center lg:bg-green-custom1">
+        <div className="lg:w-screen lg:h-screen lg:flex lg:justify-center lg:items-center lg:bg-green-custom1">
+            <Image
+                src={'/logo.svg'}
+                className="hidden lg:absolute lg:top-9 lg:left-[4rem] lg:block"
+                alt="Calendar Img"
+                width={220}
+                height={220}
+                priority
+            />
             <div className="w-full h-full bg-white-custom lg:relative lg:w-2/3 lg:max-w-[1500px] lg:h-3/4 lg:max-h-[600px] lg:rounded-2xl lg:overflow-hidden">
                 {children}
             </div>
