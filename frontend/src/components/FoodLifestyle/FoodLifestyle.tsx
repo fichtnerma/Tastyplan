512f46bc3979f3f033f7fb9f1f782e4193b040d6
fhs49302 <fhs49302@fh-salzburg.ac.at>
2023-06-17 10:47:40 +0200

    changed height of mobile view

--- frontend/src/components/FoodLifestyle/FoodLifestyle.tsx
+++ frontend/src/components/FoodLifestyle/FoodLifestyle.tsx
@@ -38,7 +38,7 @@ export default function FoodLifestyle({ onNext, onChoice, formOfDiet }: FoodLife
     return (
         <div>
             <h4 className="mb-2 h2">What is your food lifestyle?</h4>
-            <div className={styles.scrolling}>
+            <div className="flex justify-center h-[400px] lg:h-[300px] overflow-y-auto overflow-x-hidden">
                 <div className={`lg:grid-cols-2 lg:gap-x-4 lg:gap-y-5 ${styles.preferencesWrapper}`}>
                     {preferences.map((preference, i) => (
                         <div key={i} className={`mb-3 lg:m-0 h-[60px] lg:h-[80px] ${styles.choiceWrapper}`}>
--- frontend/src/components/Intolerances/Intolerances.tsx
+++ frontend/src/components/Intolerances/Intolerances.tsx
@@ -62,7 +62,7 @@ export default function Intolerances({ onNext, onBack, onChoice, allergens }: In
     return (
         <>
             <h4 className="mb-2 h2">What are your intolerances?</h4>
-            <div className="h-[300px] overflow-y-auto">
+            <div className="h-[400px] lg:h-[300px] overflow-y-auto">
                 <div className="grid grid-cols-1 gap-y-1 md:gap-y-2 md:grid-cols-1 lg:gap-y-4 lg:grid-cols-2 xl:gap-y-8 xl:grid-cols-3 2xl:gap-y-8 2xl:grid-cols-4">
                     {intolerances.map((intolerance, i) => (
                         <div
