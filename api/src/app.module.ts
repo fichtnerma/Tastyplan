b8f9db8e09c61b2193af4856d092a5e04a430da4
Markus Fichtner <markus.fichtner1999@gmail.com>
2023-06-18 22:17:07 +0200

    fix linting

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,13 +7,13 @@ import { PrismaModule } from './prisma/prisma.module';
 import { PreferencesModule } from './preferences/preferences.module';
 import { PreferencesController } from './preferences/preferences.controller';
 import { IngredientsModule } from './ingredients/ingredients.module';
+import { FavoritesModule } from './favorites/favorites.module';
 import { AuthModule } from './auth/auth.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
     imports: [
