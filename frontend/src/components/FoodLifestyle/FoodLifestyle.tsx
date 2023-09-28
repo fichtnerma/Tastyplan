4b2e6f80e13820c92c7f402e20a62e928a98b172
Manuel Proß <manuel.pross@getflip.com>
2023-09-28 15:05:10 +0200

    fix: refactor food lifestyle component to tailwind classes

--- frontend/src/components/FoodLifestyle/FoodLifestyle.tsx
+++ frontend/src/components/FoodLifestyle/FoodLifestyle.tsx
@@ -51,10 +51,11 @@ export default function FoodLifestyle({ onNext, onChoice, formOfDiet }: FoodLife
                     {preferences.map((preference, i) => (
                         <div
                             key={preference.id}
-                            className={`lg:m-0 h-[60px] lg:h-[70px] ${styles.choiceWrapper}`}
+                            className={`lg:m-0 h-[60px] lg:h-[75px] xl:h-[70px] ${styles.choiceWrapper} ${styles.customFocus}`}
                             tabIndex={i + 1}
                         >
                             <input
+                                className={`absolute top-0 right-0 bottom-0 left-0 cursor-pointer z-[1] opacity-[.01] z-[100] w-full h-full rounded-[50px] ${styles.customFocus} ${styles.radioBtn}`}
                                 type="radio"
                                 name="preferences"
                                 value={preference.food}
@@ -62,9 +63,14 @@ export default function FoodLifestyle({ onNext, onChoice, formOfDiet }: FoodLife
                                 onChange={onChoiceChange}
                                 data-cy={`${preference.food}-radio-btn`}
                             />
-                            <label htmlFor={preference.food} className="col-start-1">
-                                <p className="absolute pb-4 capitalize">{preference.food}</p>
-                                <p className="text-xs pt-8">{preference.description}</p>
+                            <label
+                                htmlFor={preference.food}
+                                className={`absolute top-0 right-0 bottom-0 left-0 cursor-pointer z-[1] flex justify-left items-center border-2 border-solid border-gray-custom3 rounded-[50px] z-[90] font-medium text-[1.124rem] leading-8 pl-8 col-start-1 ${styles.customFocus} ${styles.radioLabel}`}
+                            >
+                                <p className="absolute top-1 capitalize">{preference.food}</p>
+                                <p className="text-xs pt-6 lg:pt-8 lg:max-w-[200px] xl:max-w-[unset]">
+                                    {preference.description}
+                                </p>
                             </label>
                             <div className="flex self-center col-start-6 h-fit z-[90] center pr-4">
                                 <Icon size={50} icon={preference.icon}></Icon>
