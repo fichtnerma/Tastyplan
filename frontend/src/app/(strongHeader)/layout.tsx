14db15071bc3e5b56faf80657d56f7dbe7b66195
Manuel Proß <manuel.pross@getflip.com>
2023-10-03 15:23:54 +0200

    fix: remove unused import

--- frontend/src/app/(strongHeader)/layout.tsx
+++ frontend/src/app/(strongHeader)/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { PropsWithChildren } from 'react';
-import { SessionProvider } from 'next-auth/react';
 import SessionProviderWrapper from '@components/SessionProviderWrapper/SessionProviderWrapper';
 import SecondHeader from '@components/Layout/Header/SecondHeader/SecondHeader';
 
