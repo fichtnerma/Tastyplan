180aab12784197521b1dd09602de5e72edc1be49
Markus Fichtner <markus.fichtner1999@gmail.com>
2023-10-06 11:06:34 +0200

    remove email from auth dto

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
@@ -11,11 +11,6 @@ export class CreateUserDto {
     @IsString()
     @IsNotEmpty()
     password: string;
-
-    @ApiProperty()
-    @IsEmail()
-    @IsNotEmpty()
-    email: string;
 
     @ApiProperty({
         description: `A list of user's roles`,
