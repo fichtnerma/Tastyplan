d6482d7afd226eeb40ae571569a763b7991b9641
Tabea Schaeffer <tabea@briese-schaeffer.de>
2023-10-06 10:51:40 +0200

    weekplan styling bug fixed

--- frontend/src/app/(main)/weekOverview/page.tsx
+++ frontend/src/app/(main)/weekOverview/page.tsx
@@ -42,50 +42,61 @@ export default function WeekOverview() {
         }
     };
 
-    function showWeekplan(day: WeekplanEntry, isLunch: boolean) {
+    function showDates(day: WeekplanEntry) {
         return (
             <>
-                {weekplan.hasDinner && weekplan.hasLunch && isLunch && (
-                    <div className="sm:mb-5">
-                        <h3
-                            className="h3 !mb-0 !leading-none"
-                            style={{
-                                color: today == new Date(day.date).getDay() ? 'var(--green-dark)' : 'var(--black)',
-                            }}
-                        >
-                            {week[new Date(day.date).getDay()]}
-                        </h3>
-                        <h4
-                            className="h6 !mb-0"
-                            style={{
-                                color: today == new Date(day.date).getDay() ? 'var(--green-dark)' : 'var(--gray-3)',
-                            }}
-                        >
-                            {new Date(day.date).toLocaleDateString('de-DE', options)}
-                        </h4>
-                    </div>
-                )}
+                <div className="sm:mb-5">
+                    <h3
+                        className="h3 !mb-0 !leading-none"
+                        style={{
+                            color: today == new Date(day.date).getDay() ? 'var(--green-dark)' : 'var(--black)',
+                        }}
+                    >
+                        {week[new Date(day.date).getDay()]}
+                    </h3>
+                    <h4
+                        className="h6 !mb-0"
+                        style={{
+                            color: today == new Date(day.date).getDay() ? 'var(--green-dark)' : 'var(--gray-3)',
+                        }}
+                    >
+                        {new Date(day.date).toLocaleDateString('de-DE', options)}
+                    </h4>
+                </div>
+            </>
+        );
+    }
 
-                {isLunch ? (
-                    <RecipeCard
-                        recipe={day.lunch}
-                        highlighted={today == new Date(day.date).getDay()}
-                        withSwitch={true}
-                        smallCard={false}
-                        entryId={day.id}
-                        refreshWeekplan={refresh}
-                        isLunch={true}
-                    />
-                ) : (
-                    <RecipeCard
-                        recipe={day.dinner}
-                        highlighted={today == new Date(day.date).getDay()}
-                        withSwitch={true}
-                        smallCard={false}
-                        entryId={day.id}
-                        refreshWeekplan={refresh}
-                    />
-                )}
+    function showWeekplan(day: WeekplanEntry, hasLunch: boolean, hasDinner: boolean) {
+        return (
+            <>
+                {showDates(day)}
+
+                <div className="flex sm:block gap-4">
+                    {hasLunch && (
+                        <div className=" sm:mb-10 w-full">
+                            <RecipeCard
+                                recipe={day.lunch}
+                                highlighted={today == new Date(day.date).getDay()}
+                                withSwitch={true}
+                                smallCard={false}
+                                entryId={day.id}
+                                refreshWeekplan={refresh}
+                                isLunch={true}
+                            />
+                        </div>
+                    )}
+                    {hasDinner && (
+                        <RecipeCard
+                            recipe={day.dinner}
+                            highlighted={today == new Date(day.date).getDay()}
+                            withSwitch={true}
+                            smallCard={false}
+                            entryId={day.id}
+                            refreshWeekplan={refresh}
+                        />
+                    )}
+                </div>
             </>
         );
     }
@@ -108,37 +119,29 @@ export default function WeekOverview() {
                     </div>
                     <div className="flex">
                         {/* Mobile */}
-                        <div className="sm:hidden w-full">
-                            {weekplan?.weekplanEntry?.map((day: WeekplanEntry) => (
-                                <div key={day.date} className="mb-5">
-                                    {showWeekplan(day, true)}
-                                </div>
-                            ))}
-                        </div>
+                        {weekplan.hasLunch && (
+                            <div className="sm:hidden w-full">
+                                {weekplan?.weekplanEntry?.map((day: WeekplanEntry) => (
+                                    <div key={day.date} className="mb-5">
+                                        {showWeekplan(day, weekplan.hasLunch, weekplan.hasDinner)}
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+
                         {/* Desktop */}
 
                         <div className="">
                             {/* Lunch */}
-                            <div className="flex">
-                                {weekplan.hasLunch && <h2 className="h1 hidden sm:block">Lunch</h2>}
-
+                            <div className="flex mb-10">
+                                <div className="grid grid-cols-1 grid-rows-2">
+                                    {weekplan.hasLunch && <h2 className="h1 hidden sm:block">Lunch</h2>}
+                                    {weekplan.hasDinner && <h2 className="h1 hidden sm:block">Dinner</h2>}
+                                </div>
                                 <div className="hidden sm:flex overflow-y-hidden scrollable-element">
                                     {weekplan?.weekplanEntry?.map((day: WeekplanEntry) => (
                                         <div className="mr-6 mb-2 " key={day.date}>
-                                            {showWeekplan(day, true)}
-                                        </div>
-                                    ))}
-                                </div>
-                            </div>
-
-                            {/* Dinner */}
-                            <div className="flex">
-                                {weekplan.hasDinner && <h2 className="h1 hidden sm:block">Dinner</h2>}
-
-                                <div className="hidden sm:flex overflow-y-hidden scrollable-element">
-                                    {weekplan?.weekplanEntry?.map((day: WeekplanEntry) => (
-                                        <div className="mr-6 mb-2" key={day.date}>
-                                            {showWeekplan(day, false)}
+                                            {showWeekplan(day, weekplan.hasLunch, weekplan.hasDinner)}
                                         </div>
                                     ))}
                                 </div>
