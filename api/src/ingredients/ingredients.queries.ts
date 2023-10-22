97b4f8ce55cf25db6f38cfddd739527bc5046166
hanEck <hannes.eckelt@t-online.de>
2023-10-22 16:13:42 +0200

    refactor(api/ingredients) => Moved prisma queries into seperate directory

---
+++ api/src/ingredients/ingredients.queries.ts
@@ -0,0 +1,33 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Ingredient } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export default class IngredientsQueries {
+    constructor(private prismaService: PrismaService) {}
+
+    async upsertIngredient(ingredient: Ingredient) {
+        return await this.prismaService.ingredient.upsert({
+            where: { id: ingredient.id },
+            update: {},
+            create: {
+                id: ingredient.id,
+                name: ingredient.name.toLowerCase(),
+                categories: ingredient.categories,
+                subcategories: ingredient.subcategories,
+                calories: parseFloat(`${ingredient.calories}`) || null,
+                protein: parseFloat(`${ingredient.protein}`) || null,
+                fat: parseFloat(`${ingredient.fat}`) || null,
+                carbs: parseFloat(`${ingredient.carbs}`) || null,
+                calcium: parseFloat(`${ingredient.calcium}`) || null,
+                iron: parseFloat(`${ingredient.iron}`) || null,
+                magnesium: parseFloat(`${ingredient.magnesium}`) || null,
+                allergens: ingredient.allergens || [],
+            },
+        });
+    }
+
+    async findManyIngredients(): Promise<Ingredient[]> {
+        return await this.prismaService.ingredient.findMany();
+    }
+}
