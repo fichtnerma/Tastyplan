51f5cdca462a3557e4434fe035373e79f8f94e53
hanEck <hannes.eckelt@t-online.de>
2023-10-26 10:59:37 +0200

    refactoring(api/preferences) => Move queries into seperated file

--- api/src/preferences/preferences.module.ts
+++ api/src/preferences/preferences.module.ts
@@ -1,9 +1,10 @@
 import { PreferencesService } from './preferences.service';
+import { PreferencesQueries } from './preferences.queries';
 import { PreferencesController } from './preferences.controller';
 import { Module } from '@nestjs/common';
 
 @Module({
-    providers: [PreferencesService],
+    providers: [PreferencesService, PreferencesQueries],
     controllers: [PreferencesController],
     exports: [PreferencesService],
 })
