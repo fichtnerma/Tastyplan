5b5fd4d4f739d55f661d0f71de6c532ee59ff2c5
fhs49302 <fhs49302@fh-salzburg.ac.at>
2023-11-09 08:40:45 +0100

    wip

--- frontend/src/components/UserSettings/UserSettings.tsx
+++ frontend/src/components/UserSettings/UserSettings.tsx
@@ -4,6 +4,7 @@ function UserSettings() {
     return (
         <div className="pt-6">
             <h5>User Settings</h5>
+            <p>You don't have any user settings yet. Check back later :) </p>
         </div>
     );
 }
