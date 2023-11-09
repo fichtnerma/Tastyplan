e3f8337fdeb10d347dd7fbc44790cab7c372f0b9
fhs49302 <fhs49302@fh-salzburg.ac.at>
2023-11-09 09:22:54 +0100

    refactor dislikes

--- frontend/src/components/Dislikes/Dislikes.tsx
+++ frontend/src/components/Dislikes/Dislikes.tsx
@@ -165,28 +165,6 @@ export default function Dislikes({ onNext, onBack, onChoice, foodDislikes }: Dis
                                 handleAddChoice={handleAddChoice}
                                 allDislikes={allDislikes}
                             />
-                            {/* <TextInput
-                                placeholder="Search ingredients"
-                                value={searchTerm}
-                                decoration={
-                                    <button type="button" onClick={deleteInput}>
-                                        <Image src={cross} className="pr-1" alt="cross" width={20} priority />
-                                    </button>
-                                }
-                                decorationPosition="end"
-                                onChange={searchChanged}
-                            />
-                            <div className="relative">
-                                <div className="absolute z-1 w-full">
-                                    {searchResult.length !== 0 && isInputFocus == true && (
-                                        <SearchResultlist
-                                            searchResults={[...searchResult]}
-                                            clickHandler={handleAddChoice}
-                                            dislikes={allDislikes}
-                                        />
-                                    )}
-                                </div>
-                            </div> */}
                             <p className="inline-block text-base pt-3">Add this to your dislikes.</p>
                             <div className="flex flex-wrap">
                                 {dislikeRecommendations.map((dislike) => (
