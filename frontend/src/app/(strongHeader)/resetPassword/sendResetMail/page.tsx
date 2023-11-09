b82548eefd325a20c4d257466a70518623e5c2f7
Manuel Proß <fhs49068@fh-salzburg.ac.at>
2023-11-09 18:52:18 +0100

    feat: add page for sending password reset mail

---
+++ frontend/src/app/(strongHeader)/resetPassword/sendResetMail/page.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import React, { useState } from 'react';
+import TextInput from '@components/FormInputs/TextInput';
+import ErrorMessage from '@components/common/ErrorMessage';
+
+const sendResetMailPage = () => {
+    const [email, setEMail] = useState('');
+    const [feedbackMessage, setFeedbackMessage] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('submit');
+
+        const res = await fetch('/api/auth/reset-password', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: email }),
+        });
+
+        if (res.ok) {
+            setFeedbackMessage('Look into your mails');
+            return;
+        }
+
+        setFeedbackMessage('Mail does not exist');
+
+        console.log(res);
+    };
+
+    return (
+        <div className="lg:w-screen lg:h-[90vh] lg:flex lg:justify-center lg:items-center lg:bg-green-custom1">
+            <div className="w-full h-full bg-white-custom lg:relative lg:w-2/3 lg:max-w-[1700px] lg:h-3/4 lg:max-h-[600px] lg:rounded-[20px] lg:overflow-hidden">
+                <form
+                    className="px-10 pt-5 flex items-stretch flex-col lg:h-[90vh] lg:pt-10"
+                    action="#"
+                    onSubmit={handleSubmit}
+                >
+                    <h2 className="h1 w-full text-left">Reset your password</h2>
+                    <TextInput value={email} required onChange={setEMail} label="E-Mail" />
+                    {feedbackMessage && <p className="text-sm text-red-400">{feedbackMessage}</p>}
+                    <input className="btn-primary mt-4" type="submit" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default sendResetMailPage;
