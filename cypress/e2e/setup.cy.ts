6f4d51e45a72570e6ba4d0022ec06a25997a54c6
Manuel Proß <fhs49068@fh-salzburg.ac.at>
2023-11-12 20:29:30 +0100

    feat: add e2e test for generating weekplan as guest user

--- cypress/e2e/setup.cy.ts
+++ cypress/e2e/setup.cy.ts
@@ -6,12 +6,12 @@ describe("Setup", () => {
 
     //Login
     cy.dataCy("continue-as-guest-btn").should("exist").click();
+    cy.dataCy("decline-cookies-btn").should("exist").click();
     cy.intercept("GET", "/setup").as("login");
     cy.wait("@login");
 
     //Select food lifestyle
-    cy.dataCy("vegan-radio-btn").should("exist").check();
-    cy.dataCy("next-btn").should("exist").click();
+    cy.dataCy("vegan-radio-btn").should("exist").click({ force: true });
 
     //Select intolerances
     cy.dataCy("peanut-checkbox").should("exist").check();
@@ -22,7 +22,7 @@ describe("Setup", () => {
     //Select dislikes
     const searchTerm = "tomato";
 
-    cy.dataCy("dislikes-search-field")
+    cy.dataCy("search-ingredients")
       .should("exist")
       .type(searchTerm, { delay: 200 });
     cy.wait(200);
@@ -33,13 +33,12 @@ describe("Setup", () => {
     //Adjust weekplan
 
     //Select cooking days
-    cy.dataCy("0-checkbox").should("exist").click();
-    cy.dataCy("2-checkbox").should("exist").click();
-    cy.dataCy("4-checkbox").should("exist").click();
+    cy.dataCy("days-Monday-checkbox").should("exist").click();
+    cy.dataCy("days-Thursday-checkbox").should("exist").click();
+    cy.dataCy("days-Sunday-checkbox").should("exist").click();
 
     //Select meals
-    cy.dataCy("8-checkbox").should("exist").click();
-    cy.dataCy("9-checkbox").should("exist").click();
+    cy.dataCy("meals-Dinner-checkbox").should("exist").click();
 
     //Increase Servings
     for (let i = 0; i < 3; i++) {
@@ -49,8 +48,13 @@ describe("Setup", () => {
     cy.dataCy("portion-amount").should("exist").should("have.text", "3");
 
     //Create Weekplan
-    cy.dataCy("create-weekplan-btn").should("exist").click();
     cy.intercept("POST", "/service/weekplan/create").as("createWeekplan");
+    cy.dataCy("create-weekplan-btn").should("exist").click();
     cy.wait("@createWeekplan");
+
+    //Check if deselected days have an empty recipe card
+    cy.dataCy("0-add-recipe").should("exist");
+    cy.dataCy("1-add-recipe").should("exist");
+    cy.dataCy("4-add-recipe").should("exist");
   });
 });
