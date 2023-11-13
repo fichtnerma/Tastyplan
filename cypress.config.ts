a39091cef951b2826fbd75a28562ecc4fa772cb3
Manuel Proß <fhs49068@fh-salzburg.ac.at>
2023-11-13 19:29:51 +0100

    feat: add retries to test runs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "cypress";
-const isCI = require('is-ci')
+const isCI = require("is-ci");
 
 export default defineConfig({
   e2e: {
     //        baseUrl: 'http://193.170.119.139/',
-    baseUrl: isCI ? 'http://193.170.119.139/' : "http://localhost:8080",
+    baseUrl: isCI ? "http://193.170.119.139/" : "http://localhost:8080",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  retries: 2,
   viewportWidth: 1920,
   viewportHeight: 1080,
 });
