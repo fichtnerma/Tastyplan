43210fe12825d9b8a0f902c31bbc8e4ae761ad93
Manuel Proß <fhs49068@fh-salzburg.ac.at>
2023-11-14 18:38:06 +0100

    feat: add some comments

--- cypress/e2e/authentication.cy.ts
+++ cypress/e2e/authentication.cy.ts
@@ -13,19 +13,20 @@ describe("Authentication", () => {
     cy.dataCy("e-mail-register").type(randomEmail);
     cy.dataCy("password-register").type("123456");
     cy.dataCy("repeat-password-register").type("123456");
-
     const registerBtn = cy.dataCy("submit-register");
     registerBtn.should("not.be.disabled");
     registerBtn.click();
     cy.wait("@registerUser");
   });
+
   it("Login registered user", () => {
+    //Visit login page
     cy.visit("/authentication/login");
 
+    //Login newly created user
     cy.intercept("GET", "*/auth/*").as("loginUser");
     cy.dataCy("e-mail-login").type(randomEmail);
     cy.dataCy("password-login").type("123456");
-
     const loginBtn = cy.dataCy("submit-login");
     loginBtn.should("not.be.disabled");
     loginBtn.click();
