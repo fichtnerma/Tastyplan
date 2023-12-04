0f8750ae92e763aef2811883dc2286a8a4cc2579
fhs49302 <fhs49302@fh-salzburg.ac.at>
2023-12-04 14:24:12 +0100

    design

--- frontend/src/components/UserSettings/UserSettings.tsx
+++ frontend/src/components/UserSettings/UserSettings.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function UserSettings() {
     return (
         <div className="pt-6">
-            <h5>User Settings</h5>
+            <h5 className="mb-3">User Settings</h5>
             <p>You don't have any user settings yet. Check back later :) </p>
         </div>
     );
