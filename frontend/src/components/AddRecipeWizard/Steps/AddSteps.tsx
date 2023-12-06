d3c95f178729534069711517a42199033ced6897
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2023-12-06 11:07:05 +0100

    feat: style of step list, add functionality for adding new step

--- frontend/src/components/AddRecipeWizard/Steps/AddSteps.tsx
+++ frontend/src/components/AddRecipeWizard/Steps/AddSteps.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { CSS } from '@dnd-kit/utilities';
 import { SortableContext, arrayMove, useSortable, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import { DndContext, DragEndEvent, closestCenter } from '@dnd-kit/core';
+import TextInput from '@components/FormInputs/TextInput';
 
 type Step = {
     id: string;
@@ -15,7 +16,6 @@ const stepDummies: Step[] = [
     { id: self.crypto.randomUUID(), title: 'Step 3', description: 'This is Step 3' },
 ];
 
-console.log(stepDummies);
 type SortableStepProps = {
     step: Step;
 };
@@ -26,14 +26,29 @@ const SortableStep = ({ step }: SortableStepProps) => {
         transform: CSS.Transform.toString(transform),
     };
     return (
-        <li ref={setNodeRef} style={style} {...attributes} {...listeners} className="step">
-            <h3>{step.title}</h3>
-            <p>{step.description}</p>
+        <li
+            className="flex justify-between items-center pb-2 mb-5 border-gray-custom3 border-b-2 border-solid"
+            ref={setNodeRef}
+            style={style}
+            {...attributes}
+            {...listeners}
+        >
+            <div>
+                <h2 className="!mb-0">{step.title}</h2>
+                <p>{step.description}</p>
+            </div>
+            <div className="h-6 w-8">
+                <span className="block h-1 w-full bg-gray-custom3 mb-[4px] rounded-[2px]"></span>
+                <span className="block h-1 w-full bg-gray-custom3 rounded-[2px]"></span>
+                <span className="block h-1 w-full bg-gray-custom3 mt-[4px] rounded-[2px]"></span>
+            </div>
         </li>
     );
 };
 const AddSteps = () => {
     const [steps, setSteps] = useState<Step[]>(stepDummies);
+    const [isNewStep, setIsNewStep] = useState(false);
+    const [newStep, setNewStep] = useState<Step>({ id: '', title: '', description: '' });
     const onDragEnd = (event: DragEndEvent) => {
         const { active, over } = event;
         if (active.id === over?.id) {
@@ -47,18 +62,63 @@ const AddSteps = () => {
         });
     };
 
+    const handleTitleChange = (newTitle: string) => {
+        const step = { ...newStep };
+        step.title = newTitle;
+        setNewStep(step);
+    };
+
+    const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const step = { ...newStep };
+        step.description = e.target.value;
+        setNewStep(step);
+    };
+
+    const handleNewStep = () => {
+        const step = { ...newStep };
+        step.id = self.crypto.randomUUID();
+
+        const currentSteps = [...steps];
+        currentSteps.push(step);
+        setSteps(currentSteps);
+        setIsNewStep(false);
+    };
+
     return (
         <fieldset>
-            <legend>Add the Steps</legend>
-            <ol>
-                <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
-                    <SortableContext items={steps.map((step: Step) => step.id)} strategy={verticalListSortingStrategy}>
-                        {steps.map((step) => (
-                            <SortableStep key={step.id} step={step} />
-                        ))}
-                    </SortableContext>
-                </DndContext>
-            </ol>
+            <legend className="h1">Add the Steps</legend>
+            <div className="p-5 bg-green-custom4 rounded-[30px]">
+                <ol className="">
+                    <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+                        <SortableContext
+                            items={steps.map((step: Step) => step.id)}
+                            strategy={verticalListSortingStrategy}
+                        >
+                            {steps.map((step) => (
+                                <SortableStep key={step.id} step={step} />
+                            ))}
+                        </SortableContext>
+                    </DndContext>
+                </ol>
+                {!isNewStep ? (
+                    <button onClick={() => setIsNewStep(true)}>Add new step</button>
+                ) : (
+                    <div className="flex justify-between">
+                        <div className="flex flex-col">
+                            <TextInput required value={newStep.title} onChange={handleTitleChange} label="Title" />
+                            <label htmlFor="stepDesc">Description</label>
+                            <textarea
+                                className="mt-5"
+                                name="stepDesc"
+                                id="stepDesc"
+                                value={newStep.description}
+                                onChange={handleDescriptionChange}
+                            ></textarea>
+                        </div>
+                        <button onClick={handleNewStep}>+</button>
+                    </div>
+                )}
+            </div>
         </fieldset>
     );
 };
