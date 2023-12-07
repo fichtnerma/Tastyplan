472c1563f41ce215c66a981005633b5fe58b21e8
Markus <markus.fichtner1999@gmail.com>
2023-12-07 12:03:46 +0100

    fix(landing page): remove request

--- frontend/src/components/Startpage/Recipes/ShowRecipes.tsx
+++ frontend/src/components/Startpage/Recipes/ShowRecipes.tsx
@@ -3,15 +3,61 @@ import { Recipe } from 'src/types/types';
 import styles from '../../RecipeCard/RecipeCard.module.scss';
 
 export async function ShowRecipes() {
-    const numberOfRecipes = 5;
-    const recipes = [] as Recipe[];
-    for (let i = 1; i <= numberOfRecipes; i++) {
-        const data = await fetch(`${process.env.API_URL ? process.env.API_URL : 'http://api:3000'}/recipes/${i}`, {
-            method: 'GET',
-        });
-        const recipe = (await data.json()) as Recipe;
-        recipes.push(recipe);
-    }
+    const recipes = [
+        {
+            id: 160,
+            name: 'Greek-Style Lemon Roasted Potatoes',
+            description:
+                "These Greek potatoes wedges with lemon and oregano are roasted until tender. They're a great complement to souvlaki. All you need is a great Greek salad for a full meal!",
+            img: 'GreekStyleLemonRoastedPotatoes.jpg',
+            formOfDiet: 'omnivore',
+            preparingTime: 0,
+            cookingTime: 60,
+            totalTime: 75,
+        },
+        {
+            id: 165,
+            name: 'Chicken Yakisoba',
+            description:
+                'This traditional Japanese yakisoba noodle dish includes cabbage and chicken in a spicy sauce.',
+            img: 'ChickenYakisoba.jpg',
+            formOfDiet: 'omnivore',
+            preparingTime: 0,
+            cookingTime: 15,
+            totalTime: 35,
+        },
+        {
+            id: 133,
+            name: 'Lime Chicken Soft Tacos',
+            description: 'These chicken tacos seasoned with lime, oregano, and garlic are flavorful and delicious.',
+            img: 'LimeChickenSoftTacos.jpg',
+            formOfDiet: 'omnivore',
+            preparingTime: 0,
+            cookingTime: 30,
+            totalTime: 50,
+        },
+        {
+            id: 223,
+            name: 'Creamy Cajun Chicken Pasta',
+            description:
+                'Try this when you are feeling daring and want to mix things up a bit! A Southern inspired recipe that is sure to add a little fun to your dinner table. Try serving it with corn bread.',
+            img: 'CreamyCajunChickenPasta.jpg',
+            formOfDiet: 'omnivore',
+            preparingTime: 0,
+            cookingTime: 15,
+            totalTime: 30,
+        },
+        {
+            id: 16,
+            name: 'Vegetarian Sheet Pan Dinner with Chickpeas and Veggies',
+            description: 'Adding chickpeas to root vegetables on a sheet pan makes for an easy dinner!',
+            img: 'VegetarianSheetPanDinnerwithChickpeasandVeggies.jpg',
+            formOfDiet: 'vegan',
+            preparingTime: 0,
+            cookingTime: 45,
+            totalTime: 10,
+        },
+    ] as Recipe[];
 
     return (
         <>
