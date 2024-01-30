5f1aafc43df781a86bbf167c880d6d1e1e83113f
Manuel Proß <mpross.mmt-m2022@fh-salzburg.ac.at>
2024-01-30 20:56:06 +0100

    feat(fe): add testid

--- frontend/src/components/FormInputs/TextInput.tsx
+++ frontend/src/components/FormInputs/TextInput.tsx
@@ -63,7 +63,7 @@ export default function TextInput({
                 {decoration ? (
                     <div className={`absolute bottom-2 ${isAtStart ? 'left-2' : 'right-2'}`}>{decoration}</div>
                 ) : null}
-                <label htmlFor={id}>
+                <label htmlFor={id} data-testid={'txt-input-label'}>
                     {label}
                     {required ? ' *' : ''}
                     <input
