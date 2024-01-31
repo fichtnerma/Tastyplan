c36184e81b0d6c1c8f88b56c52db647e6663ac84
fhs49302 <fhs49302@fh-salzburg.ac.at>
2024-01-31 21:32:27 +0100

    test(frontend/mainheader): add not working unit tests

---
+++ frontend/src/components/Layout/Header/__tests__/MainHeader.test.tsx
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router';
+import { SessionProvider } from 'next-auth/react';
+import { axe } from 'jest-axe';
+import { render } from '@testing-library/react';
+import MainHeader from '../MainHeader';
+
+jest.mock('next/image', () => ({
+    __esModule: true,
+    default: (props) => {
+        return <img {...props} />;
+    },
+}));
+
+jest.mock('next/router', () => ({
+    __esModule: true,
+    useRouter: () => ({
+        pathname: '/weekOverview', // You can change this value based on your default active tab
+    }),
+}));
+
+describe('MainHeader', () => {
+    it.skip('should not have basic accessibility issues', async () => {
+        const { container } = render(
+            <SessionProvider>
+                <MainHeader />
+            </SessionProvider>,
+        );
+
+        const results = await axe(container);
+        expect(results.violations).toHaveLength(0);
+    });
+
+    it.skip('renders MainHeader component with default active tab', () => {
+        const { getByText } = render(
+            <SessionProvider>
+                <MainHeader />
+            </SessionProvider>,
+        );
+
+        const weekOverviewTab = getByText('Weekplan');
+        expect(weekOverviewTab).toHaveClass('fill-green-custom2');
+    });
+});
